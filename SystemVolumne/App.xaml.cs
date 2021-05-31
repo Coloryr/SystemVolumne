d0a309b073f876e732dedeee9232383e72466a08
Color_yr <402067010@qq.com>
2021-05-31 14:01:26 +0800

    Initial commit

---
+++ SystemVolumne/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using NAudio.CoreAudioApi;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SystemVolumne
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static MMDeviceEnumerator deviceEnumerator;
+        public static MMDevice device;
+        public static AudioEndpointVolumeChannels channels;
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            deviceEnumerator = new MMDeviceEnumerator();
+            device = deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+            channels = device.AudioEndpointVolume.Channels;
+        }
+    }
+}
---
+++ SystemVolumne/ViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemVolumne
+{
+    class ViewModel : NotificationObject
+    {
+        private int sound;
+        private int left;
+        private int right;
+        public int Sound
+        {
+            get => sound;
+            set
+            {
+                SetProperty(ref sound, value);
+                App.device.AudioEndpointVolume.MasterVolumeLevelScalar = (float)sound / 100;
+                SetProperty(ref left, (int)(App.channels[0].VolumeLevelScalar * 100), "Left");
+                SetProperty(ref right, (int)(App.channels[1].VolumeLevelScalar * 100), "Right");
+            }
+        }
+        public int Left
+        {
+            get => left;
+            set
+            {
+                SetProperty(ref left, value);
+                App.device.AudioEndpointVolume.Channels[0].VolumeLevelScalar = (float)left / 100;
+                SetProperty(ref sound, (int)(App.device.AudioEndpointVolume.MasterVolumeLevelScalar * 100), "Sound");
+            }
+        }
+        public int Right
+        {
+            get => right;
+            set
+            {
+                SetProperty(ref right, value);
+                App.device.AudioEndpointVolume.Channels[1].VolumeLevelScalar = (float)right / 100;
+                SetProperty(ref sound, (int)(App.device.AudioEndpointVolume.MasterVolumeLevelScalar * 100), "Sound");
+            }
+        }
+
+        public ViewModel()
+        {
+            Sound = (int)(App.device.AudioEndpointVolume.MasterVolumeLevelScalar * 100);
+            Left = (int)(App.channels[0].VolumeLevelScalar * 100);
+            Right = (int)(App.channels[1].VolumeLevelScalar * 100);
+        }
+    }
+}
